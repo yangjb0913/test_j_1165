799147162f0b100f7a17d5e08c487591fbb0978b
EnjoyBaby <yangjb0913@126.com>
2026-05-29 19:14:12 +0800

    Delete setup_repo.sh

--- setup_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p /testbed
-git clone --filter=blob:none --no-checkout https://github.com/jupyterhub/jupyterhub /testbed/jupyterhub
-cd /testbed/jupyterhub
-git checkout "6810aba5e9105b97a2d04eea705a4aef9f944ebb"
-cd ..
